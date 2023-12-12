1a5f778e9d74e8a043b61655f9562f2b53ec7284
Yarikdolbiev <raisindolbiev@gmail.com>
2023-12-12 19:28:24 +0200

    add files to github

---
+++ home/src/App.jsx
@@ -0,0 +1,15 @@
+import {  } from 'react'
+import Component from './Components/Component'
+
+function App() {
+ 
+
+  return (
+    
+      <div><Component/></div>
+      
+    
+  )
+}
+
+export default App
---
+++ home/src/Components/Component.jsx
@@ -0,0 +1,9 @@
+
+
+const Component = () => {
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default Component
